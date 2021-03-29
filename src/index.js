936f73e6239116ccadb0f5b551eb4b727fe26b61
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-29 15:06:45 -0600

    implemented authenticate middleware for GET to `/api/users`

--- src/index.js
+++ src/index.js
@@ -1,9 +1,84 @@
+// node modules
 const express = require("express");
-const { port } = require("./config/app.config");
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
+const cors = require("cors");
+
+// configuration
+const { port, jwt_key, salt_rounds, jwt_opt } = require("./config/app.config");
+const { models } = require("./config/mongoose.config");
+const { authenticateUser } = require("./middleware/authenticate");
+
 const app = express();
 
-app.get("/", (req, res, next) => {
-  res.send("<h1>Hello world</h1>");
+// middleware
+app.use(cors());
+app.use(express.json());
+
+/**
+ * Create a new User
+ */
+app.post("/api/users", async (req, res) => {
+  try {
+    const hashedPass = await bcrypt.hash(req.body.user.password, salt_rounds);
+    const user_params = {
+      ...req.body.user,
+      password: undefined,
+      password_digest: hashedPass,
+    };
+    const user = new models.user({ ...user_params });
+    const savedUser = await user.save();
+    await res
+      .status(201)
+      .json({ ...savedUser })
+      .send(savedUser);
+  } catch (error) {
+    // Email address already exists
+    if (error.name === "MongoError" && error.code === 11000) {
+      const error = `Account already exists with email: ${req.body.user.email}`;
+      return res.status(400).json({ error }).send();
+    }
+    // unhandled exception
+    return res.status(400).json({ error: error }).send();
+  }
+});
+
+app.get("/api/users", authenticateUser, (req, res) => {
+  console.log("headers", req.headers);
+  models.user
+    .findOne({ _id: req.headers.user_id })
+    .then((user) => {
+      const { first, last, email, day, month, year, height } = user;
+      res.json({ first, last, email, day, month, year, height }).send();
+    })
+    .catch((err) => {
+      // todo: this should handled better,
+      // error indicates no user found
+      console.error(err);
+    });
+});
+
+app.post("/api/login", async (req, res) => {
+  try {
+    const params = {
+      email: req.body.user.email,
+      password: req.body.user.password,
+    };
+    const user = await models.user.findOne({
+      email: params.email,
+    });
+    //console.debug("user", user);
+    const validPassword = await bcrypt.compare(
+      params.password,
+      user.password_digest
+    );
+    if (validPassword) {
+      const token = await jwt.sign({ id: user.id }, jwt_key, jwt_opt);
+      await res.status(200).json({ token }).send();
+    } else await res.status(401).send();
+  } catch (error) {
+    await res.status(400).json({ error: error.message }).send();
+  }
 });
 
 app.listen(port, () =>
