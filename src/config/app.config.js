4ca376ebdfc7569087c8d966cf4194bdb8cad6f6
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-29 15:05:48 -0600

    moved config for signing tokens into app config file

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,4 +1,17 @@
+require("./dotenv.config");
+
 module.exports = {
   port: process.env.NODE_ENV || 5000,
   mode: process.env.NODE_ENV || "development",
+  salt_rounds: 12,
+  jwt_key: process.env.JWT_PRIVATE_KEY,
+  jwt_opt: {
+    expiresIn: "24h",
+  },
+  mongo_url: process.env.MONGO_DB_URL,
+  google: {
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    client_secret: process.env.GOOGLE_CLIENT_SECRET,
+    callback_url: process.env.GOOGLE_CALLBACK_URL,
+  },
 };
