2aaaeecf101f6bae738e213d0964018d53062785
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-15 09:03:23 -0600

    moved constants into config file

---
+++ src/config/edamam.api.config.js
@@ -0,0 +1,10 @@
+const FOOD_BASE_URI = "https://api.edamam.com/api/food-database/v2/parser";
+const APP_ID = process.env.FOOD_API_APP_ID;
+const APP_KEY = process.env.FOOD_API_APP_KEY;
+
+
+module.exports = {
+    FOOD_BASE_URI,
+    APP_ID, 
+    APP_KEY
+}
--- src/util/edaman.api.js
+++ src/util/edaman.api.js
@@ -1,8 +1,9 @@
 const axios = require("axios").default;
-
-const FOOD_BASE_URI = "https://api.edamam.com/api/food-database/v2/parser";
-const APP_ID = process.env.FOOD_API_APP_ID;
-const APP_KEY = process.env.FOOD_API_APP_KEY;
+const {
+  APP_ID,
+  APP_KEY,
+  FOOD_BASE_URI,
+} = require("../config/edamam.api.config");
 
 const searchFoods = (text) => {
   return axios.get(`${FOOD_BASE_URI}`, {
